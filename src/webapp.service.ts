aa63e477529bc383ec5d1d63b30fca9ba0e2c0a1
Parsa <parsa.firoozi55@gmail.com>
2021-12-02 20:03:44 +0330

    Update webapp.service.ts

--- src/webapp.service.ts
+++ src/webapp.service.ts
@@ -1,8 +1,25 @@
 import express, { Application, Response, NextFunction } from 'express';
 import rateLimit from 'express-rate-limit';
 import path from 'path';
+import cors from 'cors';
 import { websiteRouter } from './routes/website.routing';
 import { get404 } from './controllers/error.controller';
+
+const allowedOrigins = ['*'];
+
+const options: cors.CorsOptions =
+  {
+    allowedHeaders: 
+      [
+        'Origin',
+        'X-Requested-With',
+        'Content-Type',
+        'Accept',
+        'X-Access-Token',
+      ],
+    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+    origin: allowedOrigins
+  };
 
 const app: Application = express(),
   limiter = rateLimit(
@@ -16,6 +33,8 @@ const app: Application = express(),
 app.set('view engine', 'ejs');
 app.set('views', (path.join(__dirname, '../views')));
 
+app.use(cors(options));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -25,6 +44,18 @@ app.use('/user', limiter);
 
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(express.json({ limit: '10kb' }));
+
+
+app.use((req: any, res: Response, next: NextFunction) =>
+{
+  req.requestTime = new Date().toISOString();
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+});
 
 app.use('/', websiteRouter);
 app.use(get404);
